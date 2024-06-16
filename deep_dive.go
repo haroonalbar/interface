0537c1f5543b6d66861a4bb85036bcec9a373cf0
Haroon Albar <haroonalbarx@gmail.com>
2024-06-16 14:58:33 +0530

    interface more complex example

---
+++ deep_dive.go
@@ -0,0 +1,70 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+	"log"
+	"time"
+
+	// postgres driver
+	_ "github.com/lib/pq"
+)
+
+type ShopModal interface {
+	CountCustomers(time.Time) (int, error)
+	CountSales(time.Time) (int, error)
+}
+
+type ShopDB struct {
+	*sql.DB
+}
+
+func (sdb *ShopDB) CountCustomers(since time.Time) (int, error) {
+	var count int
+	err := sdb.QueryRow("SELECT count(*) FROM customers WHERE timestamp > $1", since).Scan(&count)
+	return count, err
+}
+
+func (sdb *ShopDB) CountSales(since time.Time) (int, error) {
+	var count int
+	err := sdb.QueryRow("SELECT count(*) FROM sales WHERE timestamp > $1", since).Scan(&count)
+	return count, err
+}
+
+func Shop() {
+	// connect to postgres on local db deep no need for sslmode
+	db, err := sql.Open("postgres", "postgres://haroonalbar:@localhost:5432/deep?sslmode=disable")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+
+	s := &ShopDB{db}
+
+	sr, err := calculateSalesPerCustomer(s)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println(sr)
+
+}
+
+func calculateSalesPerCustomer(sm ShopModal) (string, error) {
+	// time 1 day ago
+	t := time.Now().Add(-24 * time.Hour)
+
+	customers, err := sm.CountCustomers(t)
+	if err != nil {
+		return "", err
+	}
+
+	sales, err := sm.CountSales(t)
+	if err != nil {
+		return "", err
+	}
+
+	final := float64(sales) / float64(customers)
+	return fmt.Sprintf(".2%f", final), nil
+
+}
